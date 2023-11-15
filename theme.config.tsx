7f0741a510b8bff63c29e3ba096eb40679aa6e6b
iotacb <christopher.brandt09@gmail.com>
2023-11-15 19:17:34 +0100

    Added ogi file

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,12 +6,31 @@ const config: DocsThemeConfig = {
 	project: {
 		link: "https://github.com/vallium-dev/valliumui",
 	},
-	chat: {
-		link: "https://discord.com",
-	},
+	// chat: {
+	// 	link: "https://discord.com",
+	// },
 	docsRepositoryBase: "https://github.com/vallium-dev/valliumui-doc",
 	footer: {
 		text: "Documentation page powered by Nextra",
+	},
+	head: (
+		<>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta property="og:title" content="ValliumUI Documentation" />
+			<meta
+				property="og:description"
+				content="Documentation for the ValliumUI library"
+			/>
+			<meta property="og:image" content="<generated>" />
+			<meta property="og:image:type" content="<generated>" />
+			<meta property="og:image:width" content="<generated>" />
+			<meta property="og:image:height" content="<generated>" />
+		</>
+	),
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s – ValliumUI",
+		};
 	},
 };
 
